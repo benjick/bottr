092b339dc78a29ebe71c65fd13801897e2392b1a
Max Malm <benjick@dumfan.net>
2016-02-09 22:39:43 +0100

    Uglify client bundle

--- executor/rollup.config.js
+++ executor/rollup.config.js
@@ -1,8 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'client/main.js',
   format: 'cjs',
-  plugins: [babel()],
+  plugins: [
+    babel(),
+    uglify(),
+  ],
   dest: 'src/static/bundle.js',
 };
